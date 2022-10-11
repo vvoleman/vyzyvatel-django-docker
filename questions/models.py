5c580a8844ba3d5475f9948a51a7b12bddbf1f82
bunmil <maros.mec@seznam.cz>
2022-10-11 02:36:43 +0200

    update

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 
@@ -12,9 +13,21 @@ class Category(models.Model):
         return self.name
 
 
+class Type(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    is_numeric = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, default=Category.objects.get(id=1))
+    type = models.ForeignKey(
+        Type, on_delete=models.CASCADE, default=Type.objects.get(id=1))
     question = models.CharField(max_length=255)
     right_answer = models.CharField(max_length=255)
     wrong_answer1 = models.CharField(max_length=255)
@@ -23,3 +36,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question
+
+    def get_category(self):
+        return self.category.name
+
+    def get_type(self):
+        return self.type.name
+
+    def answers(self):
+        return [self.right_answer, self.wrong_answer1, self.wrong_answer2, self.wrong_answer3]
--- questions/serializers.py
+++ questions/serializers.py
@@ -12,5 +12,4 @@ class CategorySerializer(serializers.ModelSerializer):
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ("id", "category", "is_numeric", "question",
-                  "right_answer", "wrong_answer1", "wrong_answer2", "wrong_answer3")
+        fields = ("id", "get_category", "get_type", "question", "answers")
