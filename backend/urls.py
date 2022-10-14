aacb0295a3456d2446c00b48ac65020dfb1b2d61
bunmil <maros.mec@seznam.cz>
2022-10-14 15:55:44 +0200

    email view

--- backend/urls.py
+++ backend/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('users.urls')),
     path('api/', include('questions.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework import status, authentication, permissions
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([permissions.AllowAny])
+def GetEmail(request):
+    return Response(request.user.email, status=status.HTTP_200_OK)
