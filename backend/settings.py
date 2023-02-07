aa37c6d48f6b805ea7a14a5633ac50a5d0188d8d
maros-o <maros.mec@seznam.cz>
2023-02-07 14:27:36 +0100

    deploy5

--- backend/settings.py
+++ backend/settings.py
@@ -137,3 +137,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
