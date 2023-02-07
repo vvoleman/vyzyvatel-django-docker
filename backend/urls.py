3b7f875bbd1eea9303268b3682269f4b601e1779
maros-o <maros.mec@seznam.cz>
2023-02-07 15:51:27 +0100

    deploy7

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,5 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/', include('questions.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
