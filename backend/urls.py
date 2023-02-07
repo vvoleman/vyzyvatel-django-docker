49af59983335bdba15a538856c83673d2fa9bfa2
maros-o <maros.mec@seznam.cz>
2023-02-07 15:10:30 +0100

    deploy7

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +7,4 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/auth/', include('users.urls')),
     path('api/', include('questions.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
