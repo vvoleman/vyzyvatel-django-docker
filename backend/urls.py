4ef05c41fe35ec279f3fc6e33c3c291084705a59
maros-o <maros.mec@seznam.cz>
2023-02-08 16:09:09 +0100

    urls fix

--- backend/urls.py
+++ backend/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/auth/', include('users.urls')),
     path('api/', include('questions.urls')),
+    path('', admin.site.urls),
 ]
